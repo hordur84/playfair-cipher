a53a3c833274cb3962375ef86c15fec6eac6d0b4
Hörður <hordur@envralys.is>
2022-07-06 11:00:14 +0000

    Minor changes.

--- src/playfair/play.rs
+++ src/playfair/play.rs
@@ -21,52 +21,94 @@ impl PlayfairState {
         PlayfairState { board: Board::init(data) }
     }
 
-    fn encode_row(&self, pair: [u8; 2]) -> [u8; 2] {
+    /// Returns the processed pairs that are contained within a row of `board`. A pair is encoded
+    /// within a row by increasing it's column index by 1. The decoding is the reverse.
+    /// # Arguments
+    /// 
+    /// - `pair`: Array of two values contained within a `board`.
+    /// 
+    /// # Example (encoding)
+    /// 
+    /// - `[0,2]` and `[1,4]` -> `[0,3]` and `[1,5]` resepectively. 
+    /// Given that `[0,2]` and `[1,4]` are within the same row.
+    /// ```
+    fn process_pair_row(&self, pair: [u8; 2], method: &PlayfairMethod) -> [u8; 2] {
         let mut data = [pair[0]; 2];
 
         for i in 0..pair.len() {
             let p = self.board.get_position(pair[i]).unwrap();
-            let p_updated = if p[1] == self.board.state[0].len()-1 {
-                self.board.state[p[0]][0]
-            } else {
-                self.board.state[p[0]][p[1]+1]
-            };
-            data[i] = p_updated;
+            match method {
+                PlayfairMethod::ENCODE => {
+                    let p_updated = if p[1] == self.board.state[0].len()-1 {
+                        self.board.state[p[0]][0]
+                    } else {
+                        self.board.state[p[0]][p[1]+1]
+                    };
+                    data[i] = p_updated;
+                },
+                PlayfairMethod::DECODE => {
+                    let p_updated = if p[1] == 0 {
+                        self.board.state[p[0]][self.board.state[0].len()-1]
+                    } else {
+                        self.board.state[p[0]][p[1]-1]
+                    };
+                    data[i] = p_updated;
+                }
+            }
         }
         data
     }
 
-    fn encode_column(&self, pair: [u8; 2]) -> [u8; 2] {
+    /// Returns the processed pairs that are contained within a column of `board`. A pair is encoded
+    /// within a column by increasing it's row index by 1. The decoding is the reverse.
+    /// # Arguments
+    /// 
+    /// - `pair`: Array of two values contained within a `board`.
+    /// 
+    /// # Example (encoding)
+    /// 
+    /// - `[0,2]` and `[1,4]` -> `[1,2]` and `[2,4]` resepectively. 
+    /// Given that `[0,2]` and `[1,4]` are within the same column.
+    /// ```
+    fn process_pair_column(&self, pair: [u8; 2], method: &PlayfairMethod) -> [u8; 2] {
         let mut data = [pair[0]; 2];
 
         for i in 0..pair.len() {
             let p = self.board.get_position(pair[i]).unwrap();
-            let p_updated = if p[0] == self.board.state[0].len()-1 {
-                self.board.state[0][p[1]]
-            } else {
-                self.board.state[p[0]+1][p[1]]
-            };
-            data[i] = p_updated;
+            match method {
+                PlayfairMethod::ENCODE => {
+                    let p_updated = if p[0] == self.board.state.len()-1 {
+                        self.board.state[0][p[1]]
+                    } else {
+                        self.board.state[p[0]+1][p[1]]
+                    };
+                    data[i] = p_updated;
+                },
+                PlayfairMethod::DECODE => {
+                    let p_updated = if p[0] == 0 {
+                        self.board.state[self.board.state.len()-1][p[1]]
+                    } else {
+                        self.board.state[p[0]-1][p[1]]
+                    };
+                    data[i] = p_updated;
+                }
+            }       
         }
         data
     }
 
-    fn decode_column(&self, pair: [u8; 2]) -> [u8; 2] {
-        let mut data = [0; 2];
-
-        for i in 0..pair.len() {
-            let p = self.board.get_position(pair[i]).unwrap();
-            let p_updated = if p[0] == 0 {
-                self.board.state[self.board.state.len()-1][p[1]]
-            } else {
-                self.board.state[p[0]-1][p[1]]
-            };
-            data[i] = p_updated;
-        }
-        data
-    }
-
-    fn encode_rectangle(&self, pair: [u8; 2]) -> [u8; 2] {
+    /// Returns the processed pairs that makes up a rectangle within the context of a `board`. Each pair is encoded
+    /// by modifying its column value with the other pair.
+    /// # Arguments
+    /// 
+    /// - `pair`: Array of two values contained within a `board`.
+    /// 
+    /// # Example (encoding)
+    /// 
+    /// - `[0,2]` and `[1,4]` -> `[0,4]` and `[1,2]` resepectively. 
+    /// Given that `[0,2]` and `[1,4]` make up a rectangle within the `board` context.
+    /// ```
+    fn process_pair_rectangle(&self, pair: [u8; 2]) -> [u8; 2] {
         let mut data = [pair[0]; 2];
         let p1 = self.board.get_position(pair[0]).unwrap();
         let p2 = self.board.get_position(pair[1]).unwrap();
@@ -76,41 +118,38 @@ impl PlayfairState {
         data
     }
 
-    fn encode_pair(&self, pair: [u8; 2]) -> [u8; 2] {
+    /// Returns the processed pairs depending on whether they formed a `column`, `row` or a `rectangle`.
+    /// 
+    /// # Arguments
+    /// 
+    /// - `pair`: Array of two values contained within a `board`.
+    /// - `method`: How to process the pair, can be either `encode` or `decode`.
+    /// ```
+    fn process_pair(&self, pair: [u8; 2], method: &PlayfairMethod) -> [u8; 2] {
 
         let shape = self.board.get_shape(pair);
 
         match shape {
             BoardShape::COLUMN => {
-                self.encode_column(pair)
+                self.process_pair_column(pair, method)
             },
             BoardShape::ROW => {
-                self.encode_row(pair)
+                self.process_pair_row(pair, method)
             },
             BoardShape::RECTANGLE => {
-                self.encode_rectangle(pair)
+                self.process_pair_rectangle(pair)
             }
         }
     }
 
-    pub fn decode_pair(&self, pair: [u8; 2]) -> [u8; 2] {
-        let shape = self.board.get_shape(pair);
+    /// Converts a string slice into a byte slice, and breaks it down into
+    /// pairs of two. If uneven, a byte encoded `X` is added to the last pair.
+    /// # Arguments
+    /// 
+    /// - `message`: byte encoded text message.
+    pub fn process(&self, message: &str) -> Vec<PlayfairPair> {
 
-        match shape {
-            BoardShape::COLUMN => {
-                self.decode_column(pair)
-            },
-            BoardShape::ROW => {
-                !todo!()
-            },
-            BoardShape::RECTANGLE => {
-                !todo!()
-            }
-        }
-    }
-
-    fn process(&self, message: &[u8]) -> Vec<PlayfairPair> {
-
+        let message = message.as_bytes();
         let mut data = vec![];
         let mut message = message.to_vec();
 
@@ -128,60 +167,60 @@ impl PlayfairState {
         data
     }
 
-    pub fn encode(&self, message: &[u8]) -> Vec<PlayfairPair> {
+    // pub fn encode(&self, message: &[u8], method: PlayfairMethod) -> Vec<PlayfairPair> {
 
-        let mut data = vec![];
-        let message = self.process(message);
+    //     let mut data = vec![];
+    //     let message = self.process(message);
 
-        for pair in message {
-            let encoded = self.encode_pair([pair.x, pair.y]);
-            let pair_encoded = PlayfairPair {
-                x: encoded[0],
-                y: encoded[1]
-            };
-            data.push(pair_encoded);
-        }
-        data
-    }
+    //     for pair in message {
+    //         let encoded = self.process_pair([pair.x, pair.y], &method);
+    //         let pair_encoded = PlayfairPair {
+    //             x: encoded[0],
+    //             y: encoded[1]
+    //         };
+    //         data.push(pair_encoded);
+    //     }
+    //     data
+    // }
 }
 
 pub fn main() {
 
-    const CHARS: &[u8; 25] = b"ABCDEFGHIJKLMNOPQRSTUVXYZ";
+    // const CHARS: &[u8; 25] = b"ABCDEFGHIJKLMNOPQRSTUVXYZ";
 
-    let p = PlayfairState::init(CHARS);
+    // let p = PlayfairState::init(CHARS);
 
-    let pair = ['G' as u8, 'O' as u8];
+    // let pair = ['G' as u8, 'O' as u8];
 
-    println!("{}", p.board);
+    // println!("{}", p.board);
 
-    println!("encode: {:?}", p.encode_pair(pair));
+    // //println!("encode: {:?}", p.encode_pair(pair));
 
-    let data = p.process(b"HELLOMRSVANSON");
-    for item in &data {
-        print!("{} {} ", item.x, item.y);
-    }
-    print!("\n");
-    for item in &data {
-        print!("{} {} ", item.x as char, item.y as char);
-    }
-    print!("\n");
+    // let data = p.process(b"HELLOMRSVANSON");
+    // for item in &data {
+    //     print!("{} {} ", item.x, item.y);
+    // }
+    // print!("\n");
+    // for item in &data {
+    //     print!("{} {} ", item.x as char, item.y as char);
+    // }
+    // print!("\n");
 
-    let data = p.encode(b"HELLOMRSVANSON");
-    for item in &data {
-        print!("{} {} ", item.x, item.y);
-    }
-    print!("\n");
-    for item in &data {
-        print!("{} {} ", item.x as char, item.y as char);
-    }
-    print!("\n");
+    // let data = p.encode(b"HELLOMRSVANSON", PlayfairMethod::ENCODE);
+    // for item in &data {
+    //     print!("{} {} ", item.x, item.y);
+    // }
+    // print!("\n");
+    // for item in &data {
+    //     print!("{} {} ", item.x as char, item.y as char);
+    // }
+    // print!("\n");
 
-    /* Array test */
-    let s = [[1; 3]; 2];
-    println!("s: {:?}", s);
-    println!("s row: {}", s.len());
-    println!("s col: {}", s[0].len());
+    // /* Array test */
+    // let s = [[1; 3]; 2];
+    // println!("s: {:?}", s);
+    // println!("s row: {}", s.len());
+    // println!("s col: {}", s[0].len());
 
-    println!("decode: {:?}", p.decode_pair([65, 70]))
+    // println!("decode: {:?}", p.process_pair([67, 76], &PlayfairMethod::DECODE));
 }
